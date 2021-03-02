0c460921d9414b099e8ecb4b0df256ef854fec69
msarker1 <sarkermt1@gmail.com>
2021-03-02 01:00:15 -0500

    fix(validation): fixed validation util method

--- src/main/java/com/sarker/commons/constant/CommonsConstants.java
+++ src/main/java/com/sarker/commons/constant/CommonsConstants.java
@@ -6,15 +6,23 @@ package com.sarker.commons.constant;
  *
  */
 public class CommonsConstants {
+	
 	public static final String UNDERSCORE = "_";
+	
 	public static final String FILE_TYPE_JSON = ".json";
+	
 	public static final String PARSER = "@";
+	
 	public static final String GCM = "AES/GCM/NoPadding";
+	
 	public static final String AES = "AES";
+	
 	public static final String HMAC256 = "HmacSHA256";
-	public static final String IPV4REGEX = "((\\d)|([\\d][\\d])|([01][\\d][\\d])|([2][0-5][0-5]))\\." +
-			"((\\d)|([\\d][\\d])|([01][\\d][\\d])|([2][0-5][0-5]))\\."+
-			"((\\d)|([\\d][\\d])|([01][\\d][\\d])|([2][0-5][0-5]))\\."+
-			"((\\d)|([\\d][\\d])|([01][\\d][\\d])|([2][0-5][0-5]))";
-	public static final String IPV6REGEX = "(([0-9a-zA-Z]{1,4}:){7}[0-9a-zA-Z]{1,4})";
+	
+	public static final String IPV4REGEX = "^((\\d)|([\\d][\\d])|([01][\\d][\\d])|([2][0-4][\\d])|([2][0-5][0-5]))\\." +
+			"((\\d)|([\\d][\\d])|([01][\\d][\\d])|([2][0-4][\\d])|([2][0-5][0-5]))\\."+
+			"((\\d)|([\\d][\\d])|([01][\\d][\\d])|([2][0-4][\\d])|([2][0-5][0-5]))\\."+
+			"((\\d)|([\\d][\\d])|([01][\\d][\\d])|([2][0-4][\\d])|([2][0-5][0-5]))$";
+	
+	public static final String IPV6REGEX = "^(([0-9a-zA-Z]{1,4}\\:){7}[0-9a-zA-Z]{1,4})$";
 }
